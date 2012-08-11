3250984f99e5e205563572d5f2534f7ceb7ccd0b
Paul Wood <paul@Paul-Woods-iMac.local>
2012-08-11 08:28:01 +0800

    Initial Commit

---
+++ WeiboKit/PWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PWAppDelegate.h
+//  WeiboKit
+//
+//  Created by Paul Wood on 8/11/12.
+//  Copyright (c) 2012 Paul Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeiboKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeiboKit
+//
+//  Created by Paul Wood on 8/11/12.
+//  Copyright (c) 2012 Paul Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PWAppDelegate class]));
+    }
+}
