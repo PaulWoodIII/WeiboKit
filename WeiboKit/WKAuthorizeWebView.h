fbab7f044487607220043f5460ccd5baf0c3edd6
Paul Wood <paul@Paul-Woods-iMac.local>
2012-08-15 10:09:02 +0800

    Added WKAuthorize and its WebView for OAuth2 authentication They are not yet used by the OAuthClient

---
+++ WeiboKit/WKAuthorizeWebView.h
@@ -0,0 +1,42 @@
+//
+//  WKAuthorizeWebView.h
+//  WeiboKit
+//
+//  Created by Paul Wood on 8/15/12.
+//  Copyright (c) 2012 Paul Wood. All rights reserved.
+//
+//  Note:
+//  Inspired by the WBAuthorizeWebView
+//  Modified greatly for ARC support and integration to WeiboKit
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class WKAuthorizeWebView;
+
+@protocol WKAuthorizeWebViewDelegate <NSObject>
+
+- (void)authorizeWebView:(WKAuthorizeWebView *)webView didReceiveAuthorizeCode:(NSString *)code;
+
+@end
+
+@interface WKAuthorizeWebView : UIView <UIWebViewDelegate> 
+{
+    UIView *panelView;
+    UIView *containerView;
+    UIActivityIndicatorView *indicatorView;
+	UIWebView *webView;
+    
+    UIInterfaceOrientation previousOrientation;
+    
+}
+
+@property (nonatomic, assign) id<WBAuthorizeWebViewDelegate> delegate;
+
+- (void)loadRequestWithURL:(NSURL *)url;
+
+- (void)show:(BOOL)animated;
+
+- (void)hide:(BOOL)animated;
+
+@end
